84d53116da31562b0324a8b0c27e71e37f71adb4
Anton <drobyshev.anton@ashmanov.net>
2020-11-09 14:11:45 +0300

    Fixed config path

--- models.py
+++ models.py
@@ -2,7 +2,7 @@ from config import Config
 from modules.tts.synthesizer import Synthesizer
 
 
-config = Synthesizer.load_config("config.yaml")
+config = Synthesizer.load_config("data/config.yaml")
 
 natasha = Synthesizer.from_config(config, name="natasha")
 ruslan = Synthesizer.from_config(config, name="ruslan")
